2c9b4adb178f95720cde22f3eabcf220d197ee9b
Juan Manuel <juanma.caaz@gmail.com>
2022-11-07 11:39:24 +0100

    Test get ranking

--- test/test_ranking_model.py
+++ test/test_ranking_model.py
@@ -20,9 +20,19 @@ def test_add_ranking():
     assert(miguel[0][0] == 'Miguel')
     assert(miguel[0][1] == 1)
 
+
 def test_delete_all():
     model.add_ranking('Miguel', 10)
     model.add_ranking('Paco', 20)
     model.delete_all()
     res = model.get_ranking()
     assert(len(res) == 0)
+
+
+def test_get_ranking():
+    model.delete_all()
+    model.add_ranking('Miguel', 10)
+    res = model.get_ranking()
+    assert(type(res) == list)
+    assert(res[0][0] == 'Miguel')
+    assert(res[0][1] == 10)
