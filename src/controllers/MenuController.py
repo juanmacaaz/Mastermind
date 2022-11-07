e40f19c6004cb5eb384e9df79b0da386749661cf
Juan Manuel <juanma.caaz@gmail.com>
2022-11-07 11:19:48 +0100

    Tests menu_controllers OK

--- src/controllers/MenuController.py
+++ src/controllers/MenuController.py
@@ -1,4 +1,28 @@
 from controllers.Controller import Controller
 
 class MenuController(Controller):
-    pass
+
+    def __init__(self) -> None:
+        self.menu = {
+            1: ['Play game', 'game', 'show_difficulty'],
+            2: ['Ranking', 'ranking', 'show_ranking'],
+            3: ['Exit', 'menu', 'show_exit'],
+        }
+
+    def show_menu(self, data = {}):
+        return {'view': 'MenuView', 'data': data}
+
+    def show_exit(self, data = {}):
+        return {'view': 'ExitView', 'data': data}
+
+    def select_option(self, data):
+        try:
+            option = int(data['option'])
+        except ValueError:
+            return {'view': 'MenuView', 'data': {'error': 'Invalid Type'}}
+        if option not in self.menu:
+            return self.show_menu({'error': 'Invalid option'})
+        return self.controllers[self.menu[option][1]].__getattribute__(self.menu[option][2])({})
+
+    def exit(self, _):
+        exit()
