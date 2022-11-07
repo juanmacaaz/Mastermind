6147e1a0efa22bc56179e6f8e10eb217984d714f
Juan Manuel <juanma.caaz@gmail.com>
2022-11-07 11:37:05 +0100

    Test delete all and add ranking

--- test/test_ranking_model.py
+++ test/test_ranking_model.py
@@ -3,8 +3,26 @@ sys.path.insert(0, 'src')
 
 from models.RankingModel import RankingModel
 
+model = RankingModel.get_instance()
+
 def test_singleton():
     try:
         RankingModel()
     except Exception:
         assert(True)
+
+def test_add_ranking():
+    model.delete_all()
+    model.add_ranking('Miguel', 1)
+    res = model.get_ranking()
+    miguel = [x for x in res if x[0] == 'Miguel']
+    assert(len(miguel) == 1)
+    assert(miguel[0][0] == 'Miguel')
+    assert(miguel[0][1] == 1)
+
+def test_delete_all():
+    model.add_ranking('Miguel', 10)
+    model.add_ranking('Paco', 20)
+    model.delete_all()
+    res = model.get_ranking()
+    assert(len(res) == 0)
