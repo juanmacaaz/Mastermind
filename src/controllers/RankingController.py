0756619a598a8831c25b6e5d10df34118db01a21
Juan Manuel <juanma.caaz@gmail.com>
2022-11-08 23:08:54 +0100

    Finish Ranking controller

--- src/controllers/RankingController.py
+++ src/controllers/RankingController.py
@@ -9,3 +9,15 @@ class RankingController(Controller):
     def show_ranking(self, _):
         ranking = self.ranking_model.get_ranking()
         return {'view': 'RankingView', 'data': ranking}
+    
+    def add_ranking(self, data):
+        # Check alphanumerical
+        if not data['name'].isalnum():
+            return {'view': 'WinnerView',
+                    'data': self.ranking_model.get_ranking(),
+                    'error': 'El nombre debe ser alfanumerico'}
+        intentos = self.controllers['game'].get_n_intentos()
+        dificultad = self.controllers['game'].get_difficulty()
+        score = intentos * (dificultad + 1)
+        self.ranking_model.add_ranking(data['name'], score)
+        return {'view': 'MenuView', 'data': {}}
--- test/test_ranking_controller.py
+++ test/test_ranking_controller.py
@@ -10,3 +10,36 @@ controller = RankingController()
 def test_show_ranking():
     res = controller.show_ranking(None)
     assert(res['view'] == 'RankingView')
+
+# Prova add ranking
+def test_add_ranking(mocker):
+    game_controller = GameController()
+    game_controller.get_n_intentos = mocker.MagicMock(return_value=4)
+    game_controller.get_difficulty = mocker.MagicMock(return_value=1)
+
+    controller.set_controllers({'game': game_controller})
+    controller.ranking_model.delete_by_name('Miguel')
+    res_ = controller.add_ranking({'name': 'Miguel'})
+    res = controller.ranking_model.get_ranking()
+    miguel = [x for x in res if x[0] == 'Miguel']
+
+    assert(len(miguel) == 1)
+    assert(miguel[0][1] == 8)
+    assert(res_['view'] == 'MenuView')
+
+
+def test_add_not_alphanumerical_ranking(mocker):
+    game_controller = GameController()
+    game_controller.get_n_intentos = mocker.MagicMock(return_value=4)
+    game_controller.get_difficulty = mocker.MagicMock(return_value=1)
+
+    controller.set_controllers({'game': game_controller})
+    controller.ranking_model.delete_all()
+    res_ = controller.add_ranking({'name': ' Mi;guel*"\''})
+    # Get ranking
+    res = controller.ranking_model.get_ranking()
+    # Find Miguel in ranking
+    miguel = [x for x in res if x[0] == ' Mi;guel*"\'']
+    # Check if Miguel is in ranking
+    assert(len(miguel) == 0)
+    assert(res_['view'] == 'WinnerView')
