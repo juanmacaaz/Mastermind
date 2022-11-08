50cff7f4e79bcec07b697db233308143be997c06
Juan Manuel <juanma.caaz@gmail.com>
2022-11-08 23:26:41 +0100

    Show difficulty test

--- src/controllers/GameController.py
+++ src/controllers/GameController.py
@@ -2,3 +2,6 @@ from controllers.Controller import Controller
 
 class GameController(Controller):
     pass
+
+    def show_difficulty(self, data = {}):
+        return {'view': 'DifficultyView', 'data': data}
